8cb9baba1df016895f8a962cb8f3963b9da394dd
Mahe2patel <patelmahetab9020@gmail.com>
2024-11-27 00:13:48 +0530

    Admin DashBoard Page will be Created..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Login from './Components/Authentication/Login';
 import TeamMember from './Components/DashBoard/TeamMember';
-
+import AdminDashboard from './Components/DashBoard/AdminDashboard';
 export default function App() {
   return (
     <>
     {/* <Login/> */}
-    <TeamMember/>
-  
+    {/* <TeamMember/> */}
+  <AdminDashboard/>
     </>
   )
 }
